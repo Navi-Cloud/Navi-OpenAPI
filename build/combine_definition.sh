f572de2ea56959ad7766c994aa82890edce77afb
KangDroid <hyunwoo286@gmail.com>
2022-04-17 01:30:27 +0900

    Feature: Add support for Navi-Storage

--- build/combine_definition.sh
+++ build/combine_definition.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 swagger-cli bundle auth/NaviAuthDefinition.yaml -o /swagger/out/auth-bundle.yaml -t yaml
+swagger-cli bundle storage/NaviStorageDefinition.yaml -o /swagger/out/storage-bundle.yaml -t yaml
